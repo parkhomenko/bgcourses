1861199201fdc293d997596f750ba87d7a119c95
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2017-03-11 21:05:15 +0200

    intermediate commit

--- task2/docker/docker-entrypoint.sh
+++ task2/docker/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo "UserKnownHostsFile=/dev/null" >> /etc/ssh/ssh_config
+
+echo "yes \n" | service ssh restart
+echo "yes \n" | ssh localhost
 /usr/local/hadoop/bin/hadoop namenode -format
 /usr/local/hadoop/sbin/start-dfs.sh
 
