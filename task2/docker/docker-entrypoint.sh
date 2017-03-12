8e631597a5211d5069d8ff8ebc0860c82a0c258c
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2017-03-12 17:28:03 +0200

    hdfs single node docker container

--- task2/docker/docker-entrypoint.sh
+++ task2/docker/docker-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-echo "UserKnownHostsFile=/dev/null" >> /etc/ssh/ssh_config
-
-echo "yes \n" | service ssh restart
-echo "yes \n" | ssh localhost
-/usr/local/hadoop/bin/hadoop namenode -format
+service ssh restart
 /usr/local/hadoop/sbin/start-dfs.sh
 
 if [[ $1 == "-d" ]]; then
